4132d6a753bc6468e70c14f13da8236312905817
unknown <kristiana.ingco@obf.ateneo.edu>
2019-02-26 17:09:23 +0800

    Added partials
    Integrated bootstrap and created blade php partials for reuse

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Soul Cone</title>
+
+      	<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+
